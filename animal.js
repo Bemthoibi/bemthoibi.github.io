c19d859a5ab2468277b183260fdbd4aebcf01c5f
Bemthoibi Singha <bemthoibi31@gmail.com>
2024-08-23 21:47:46 +0530

    Add files via upload

---
+++ animal.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener('click', function() {
+    
+    setTimeout(function() {
+        window.location.href  = 'cutie.jpeg'; //joining birthday.html, my main page
+    },0); 
+});
